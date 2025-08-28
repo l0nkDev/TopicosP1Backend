115bd46f882c8acd8d3055ed143181d1e6a5a1f0
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-08-28 19:11:21 -0400

    eeee soy sans

--- TopicosP1Backend/Program.cs
+++ TopicosP1Backend/Program.cs
@@ -19,11 +19,11 @@ builder.Services.AddSingleton<APIQueue>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
